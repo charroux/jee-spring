50e5fdd83719eebffa47863d95ac9e4890fe1f7d
charroux <benoit.charroux@gmail.com>
2023-01-12 14:57:32 +0100

    web service integration

--- src/main/java/com/example/demo/CarRepository.java
+++ src/main/java/com/example/demo/CarRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 public interface CarRepository extends CrudRepository<Car, Integer> {
 
     public Car findByMarque(String marque);
+    public Car findByPlaque(String plaque);
 
 }
--- src/main/java/com/example/demo/LocationControllerJPA.java
+++ src/main/java/com/example/demo/LocationControllerJPA.java
@@ -1,7 +1,61 @@
 package com.example.demo;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class LocationControllerJPA {
+
+    @Autowired
+    CarRepository carRepository;
+
+    @GetMapping("/jpa/cars")
+    public Iterable<Car> getCars(){
+        return carRepository.findAll();
+    }
+
+    /**
+     * http://localhost:8080/cars/11AABB
+     * @param immat
+     * @return
+     */
+    @GetMapping("/jpa/cars/{plaque}")
+    public Car getCar(@PathVariable("plaque") String immat) throws CarNotFoundException {
+        Car car = carRepository.findByPlaque(immat);
+        if(car == null){
+            throw new CarNotFoundException("Voiture non trouvée : " + immat);
+        }
+        return car;
+    }
+
+
+    /**
+     * http://localhost:8080/cars?louer=true
+     */
+    @PutMapping("/jpa/cars/{plaque}")
+    public Car louer(@RequestParam("louer") boolean rent,
+                     @PathVariable("plaque") String immat,
+                     @RequestBody Dates dates) throws CarNotFoundException {
+
+        Car car = carRepository.findByPlaque(immat);
+        if(car == null){
+            throw new CarNotFoundException("Voiture non trouvée : " + immat);
+        }
+        car.setRent(rent);
+        car.setDates(dates);
+        return car;
+    }
+
+    /**
+     * Transmettre des données dans la requête
+     */
+    @PostMapping("jpa//cars")
+    public void creerVoiture(@RequestBody Car car){
+        System.out.println(car);
+        // A faire : ajouter une voiture
+    }
+
+
 }
