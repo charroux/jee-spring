d1495f1666c1f1f9d62a0e290c88e5d92f2e9e69
charroux <benoit.charroux@gmail.com>
2023-01-12 16:43:36 +0100

    json

--- src/main/java/com/example/demo/Dates.java
+++ src/main/java/com/example/demo/Dates.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
 
 @Entity
@@ -41,6 +43,7 @@ public class Dates {
     }
 
     @OneToOne
+    @JsonIgnore
     public Car getCar() {
         return car;
     }
